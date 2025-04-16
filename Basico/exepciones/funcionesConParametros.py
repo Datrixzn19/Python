7a68bee72afc65ff97a8ed8a042211b2bf808781
David19 <davidleonel911@gmail.com>
2025-04-15 20:52:49 -0500

    funciones con parametros

---
+++ Basico/exepciones/funcionesConParametros.py
@@ -0,0 +1,21 @@
+def saludar(nombre):
+    print(f"¡Hola, {nombre}!")
+
+saludar("David")  #aqui pasamos el parametro que nos pide la funcion
+
+
+#También puedes asignar valores predeterminados para evitar errores si no se pasa un argumento:
+def saludar(nombre="Usuario"):#usuaio sera el valor predeterminado
+    print(f"¡Hola, {nombre}!")
+
+saludar()  
+
+
+    #Retorno de valores 
+def sumar(a, b):
+    return a + b  # Retorna la suma
+
+print(sumar(10, 5))
+# resultado = sumar(10, 5)
+# print(resultado)  # Salida: 15
+
