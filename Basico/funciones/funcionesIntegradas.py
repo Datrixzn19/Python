5ac4f0f96d88e487ffd5074e1a24cdec1113155e
David19 <davidleonel911@gmail.com>
2025-04-17 19:53:50 -0500

    funciones integradas

---
+++ Basico/funciones/funcionesIntegradas.py
@@ -0,0 +1,22 @@
+"""
+Tambien llamadas builtin functions 
+
+son funciones predefinidas
+siempre estan disponibles y nos sirven para construir funciones mas complejas 
+
+abs(x): Retorna el valor absoluto de un número.
+
+len(iterable): Devuelve la cantidad de elementos en un iterable.
+
+max(iterable): Encuentra el valor máximo en una colección de datos.
+
+min(iterable): Encuentra el valor mínimo.
+
+sum(iterable): Suma los elementos de un iterable.
+
+sorted(iterable): Ordena los elementos de un iterable.
+
+type(obj): Retorna el tipo de un objeto.
+
+"""
+#
