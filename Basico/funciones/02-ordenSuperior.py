7eb93bf026df45d8bf6ba33a560040d131cc0d50
David19 <davidleonel911@gmail.com>
2025-04-18 21:09:03 -0500

    funciones de orden superior

--- Basico/funciones/02-ordenSuperior.py
+++ Basico/funciones/02-ordenSuperior.py
@@ -1,8 +1,9 @@
 #ahora vamos a ver la func devuelve una funcion como resultado 
 
-def divisor(x):
-    def dividendo(y):
-        return y / x
-    return dividendo
-divide = divisor(2)
-print(divide(10))
+def divisor(x):  # Definimos la función externa que recibe un argumento x
+    def dividendo(y):  # Definimos la función interna que recibe otro argumento y
+        return y / x  # La función interna usa el valor de x
+    return dividendo  # Retornamos la función interna sin ejecutarla
+
+divide = divisor(2)  # Llamamos a divisor(2), que retorna la función 'dividendo' con x = 2
+print(divide(10))  # Llamamos a la función retornada con el argumento 10, por lo que 10 / 2 = 5
