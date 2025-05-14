99a2b8362689a5466c2ad8b588a95a8d779dd923
David19 <davidleonel911@gmail.com>
2025-05-14 18:00:36 -0500

    logica

--- Basico/EjerciciosDeLogica.py/numeroPerdido.py
+++ Basico/EjerciciosDeLogica.py/numeroPerdido.py
@@ -1,5 +1,13 @@
 #encuentra el numero faltante en una lista desordenada sin usar el metodo sort
 
-#[2,5,8,6,3,7]   falta el 4
+#PD solo funciona si la lista empieza por 1 jajaj
 
-listaDesorneda = [2,5,8,6,3,7]
+def encontrar_faltante(lista, n):
+    suma_esperada = n * (n + 1) // 2  #esto lo que hace es dar la suma de la lista solo usando el numero mayor 
+    suma_real = sum(lista)  # Suma de los números presentes en la lista
+    return suma_esperada - suma_real  #restando la suma de todos los numeros menos la suma de la lista incompleta obtenemos el numero faltante
+
+# Ejemplo de uso
+numeros = [4, 1, 7, 6, 2, 5]  # Falta el 3
+n = 7  #el numero mayor de la lista
+print("Número faltante:", encontrar_faltante(numeros, n))
